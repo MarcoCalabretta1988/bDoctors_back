f41ece9ea2c6faa4134f56b672188d3b65094c29
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-31 14:25:18 +0200

    add corelations and migrations

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,10 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class Doctor extends Model
 {
     use HasFactory;
-
+    //corelations user
     protected $fillabale = ['phone', 'address', 'curriculum', 'photo'];
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+    //corelation mesage one to many 
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    //correlation whith review one to one 
+
+    public function review()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    //corelations many to many votes
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+    //corelations many to many sponsoreds
+    public function sponsoreds()
+    {
+        return $this->hasMany(Sponsored::class);
+    }
+    //corelations many to many specializations
+    public function specializations()
+    {
+        return $this->hasMany(Specialization::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //corelations doctor
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
