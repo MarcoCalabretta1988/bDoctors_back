a3a50ce955a2628e372d9b5b464606741787fff1
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-04-06 18:06:13 +0200

    if no message in index

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -22,28 +22,29 @@
                   </tr>
                 </thead>
                 <tbody >
-                  
-                    @foreach ($messages as $message)
-                    <tr class="{{ $message->is_read ? "text-secondary" : "text-white"}}">    
-                    <th scope="row">{{$message->name}}</th>
-                    <td>{{$message->email}}</td>
-                    <td>{{$message->text}}</td>
-                    <td>{{$message->created_at}}</td>
-    
-                    <td>
-                      <div class="button-box d-flex justify-content-end">
-                        <a href="{{route('admin.messages.show',$message->id)}}" class="btn btn-sm btn-primary me-3"><i class="fa-sharp fa-solid fa-eye"></i></a>
-                       
-              
-                         <form action="{{ route('admin.messages.destroy' , $message->id)}}" method="POST" class="trash-form">
-                          @method('DELETE')
-                          @csrf
-                          <button  technology="submit" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
-                         </form>
-                      </div>
-                      </td>
-                  </tr>
-                    @endforeach
+                  @if (!$messages[0])
+                  <h1 class="p-blu t-shadow">Non ci sono nuovi messaggi !</h1>                      
+                  @else
+                  @foreach ($messages as $message)
+                <tr class="{{ $message->is_read ? "text-secondary" : "text-white"}}">    
+                  <th scope="row">{{$message->name}}</th>
+                  <td>{{$message->email}}</td>
+                  <td>{{$message->text}}</td>
+                  <td>{{$message->created_at}}</td>
+  
+                  <td>
+                    <div class="button-box d-flex justify-content-end">
+                      <a href="{{route('admin.messages.show',$message->id)}}" class="btn btn-sm btn-primary me-3"><i class="fa-sharp fa-solid fa-eye"></i></a>
+                      <form action="{{ route('admin.messages.destroy' , $message->id)}}" method="POST" class="trash-form">
+                        @method('DELETE')
+                        @csrf
+                        <button  technology="submit" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+                      </form>
+                    </div>
+                    </td>
+                </tr>
+                  @endforeach
+                  @endif
                 </tbody>
               </table>
               <div class="d-flex  align-items-center">
