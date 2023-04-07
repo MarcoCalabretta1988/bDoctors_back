6cd3ed6586d224951cc401738d102d3af891d452
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-04-07 18:11:31 +0200

    css for payment form

--- resources/views/admin/doctors/paymentForm.blade.php
+++ resources/views/admin/doctors/paymentForm.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="login-bg">
+    <div class="login-bg pt-5">
         {{-- error bag --}}
         @if ($errors->any())
             <div class="alert alert-danger my-3" role="alert">
@@ -12,29 +12,50 @@
             </div>
         @endif
 
-        <div class="container bg-light welcome-box">
+        <div class="container bg-light welcome-box rounded">
 
             <form action="{{ route('admin.doctors.updatepro', $doctor->id) }}" method="POST" class="row g-3 py-5" novalidate>
                 @method('PUT')
                 @csrf
-
+                {{-- NUMERO CARTA --}}
                 <div class="col-md-4">
-                    <label for="validationDefault01" class="form-label">Numero carta</label>
+                    <div class="title-wrapper d-flex justify-content-between">
+                        <label for="validationDefault01" class="form-label">Numero carta</label>
+                        <div class="card-logo-wrapper d-flex">
+                            {{-- LOGHI CARTE --}}
+                            <div class="card-int">
+                                <img src="{{url('/img/MS-logo.png')}}" class="img-fluid" alt="circuit logo">
+                            </div>
+                            <div class="card-int">
+                                <img src="{{url('/img/visa-logo.png')}}" class="img-fluid" alt="circuit logo">
+                            </div>
+                            <div class="card-int">
+                                <img src="{{'/img/AME-logo.png'}}" class="img-fluid" alt="circuit logo">
+                            </div>
+                        </div>
+                    </div>
                     <input type="text" class="form-control" placeholder="1234 5678 9102 3456" required>
                 </div>
+                {{-- SCADENZA --}}
                 <div class="col-md-4">
                     <label for="validationDefault02" class="form-label">Data di scadenza</label>
                     <input type="date" class="form-control" required>
                 </div>
-
-                <div class="col-md-3">
-                    <label for="validationDefault03" class="form-label">CVC</label>
-                    <input type="number" placeholder="123" class="form-control" required>
+                {{-- CVv --}}
+                <div class="py-4 d-flex">
+                    <div class="col-md-3 me-5">
+                        <label for="validationDefault03" class="form-label">CVV</label>
+                        <input type="number" placeholder="123" class="form-control" required>
+                    </div>
+                    <div class="col-3 cvv">
+                        <img src="{{url('/img/cvv-icon-27.jpg')}}" class="img-fluid " alt="cvv illustration">
+                    </div>
                 </div>
+                {{-- PIANO --}}
                 <div class="col-md-3">
-                    <label for="validationDefault03" class="form-label">Scegli piano</label>
+                    <label for="validationDefault03" class="form-label">Scegli un piano</label>
                     <select class="form-select" id="validationDefault04" name="sponsored_ad">
-                        <option value="" selected>Scegli un piano di sponsorizazione</option>
+                        <option value="" selected>---</option>
                         @foreach ($sponsoreds as $sponsored)
                             <option value="{{ $sponsored->id }}">{{ $sponsored->name }} costo:
                                 {{ $sponsored->cost }}€</option>
@@ -42,7 +63,7 @@
 
                     </select>
                 </div>
-
+                {{-- TERMINI E CONDIZIONI --}}
                 <div class="col-12">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
