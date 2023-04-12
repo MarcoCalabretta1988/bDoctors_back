695fea2cc1e1094f296b81a30900b3a42e10137b
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-04-12 16:31:11 +0200

    braintree

---
+++ braintreeTutorial/routes/checkout.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'ksvtdb5kdz5pqz3k',
+    publicKey: 'w322n2cr8k3bw7nj',
+    privateKey: 'b39462e5f20e7094e3fd93b476f1aa5c'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
+
+module.exports = router;
