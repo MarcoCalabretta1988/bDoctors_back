5fb90898fbb4e801da7a4e5d1d987da2dcb883f3
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-04-12 17:47:55 +0200

    make dinamic sponosred

--- resources/views/admin/doctors/sponsored.blade.php
+++ resources/views/admin/doctors/sponsored.blade.php
@@ -7,95 +7,40 @@
     <hr>
 
     <div class="row justify-content-center align-items-center">
-    <div class="col-4 text-center">
-
-            <div class="column-title">
-            <h3>Bronze</h3>
-               <hr>
-            </div>
-            <div>
-               
-               <ul>
-                   <li class="flex">
-                       <span><i class="fa-regular fa-clock"></i></span> 
-                       <span>24 Ore</span>
-                   </li>
-                   <li class="flex">
-                    <span><i class="fa-solid fa-money-bill-wave"></i></span> 
-                    <strong>€ 2,99</strong>
-                </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-eye"></i></span>
-                        <span>Presenza in home page</span>
-                   </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-ranking-star"></i> </span>
-                       <span>Appari tra i primi risultati della ricerca</span></li>
-                   <li><a href="{{route('admin.doctors.paymentForm' , 1)}}" class="btn btn-primary">Acquista ora!</a></li>
+        @foreach ($sponsoreds as $sponsored )
+        <div class="col-4 text-center">
+    
+                <div class="column-title">
+                <h3>{{$sponsored->name}}</h3>
+                   <hr>
+                </div>
+                <div>
+                   
+                   <ul>
+                       <li class="flex">
+                           <span><i class="fa-regular fa-clock"></i></span> 
+                           <span>{{$sponsored->duration}} Ore</span>
+                       </li>
+                       <li class="flex">
+                        <span><i class="fa-solid fa-money-bill-wave"></i></span> 
+                        <strong>{{$sponsored->cost}}</strong>
+                    </li>
+                       <li class="flex">
+                           <span><i class="fa-solid fa-eye"></i></span>
+                            <span>Presenza in home page</span>
+                       </li>
+                       <li class="flex">
+                           <span><i class="fa-solid fa-ranking-star"></i> </span>
+                           <span>Appari tra i primi risultati della ricerca</span></li>
+                       <li><a href="{{route('admin.doctors.paymentForm' , $sponsored->id)}}" class="btn btn-primary">Acquista ora!</a></li>
+                
+                   </ul>
+                   <hr>
+                </div>
+        </div>
             
-               </ul>
-               <hr>
-            </div>
-    </div>
-    <div class="col-4  text-center">
-        <div class="column-title">
-            <h3>Medium</h3>
-               <hr>
-            </div>
-            <div>
-               
-               <ul>
-                   <li class="flex">
-                       <span><i class="fa-regular fa-clock"></i></span> 
-                       <span>72 Ore</span>
-                   </li>
-                   <li class="flex">
-                    <span><i class="fa-solid fa-money-bill-wave"></i></span> 
-                    <strong>€ 5,99</strong>
-                </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-eye"></i></span>
-                        <span>Presenza in home page</span>
-                   </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-ranking-star"></i> </span>
-                       <span>Appari tra i primi risultati della ricerca</span></li>
-                   <li><a href="{{route('admin.doctors.paymentForm' , 2)}}" class="btn btn-primary">Acquista ora!</a></li>
-            
-               </ul>
-               <hr>
-            </div>
-    </div>
-
-    <div class="col-4  text-center"> 
-        <div class="column-title">
-            <h3>Top</h3>
-               <hr>
-            </div>
-            <div>
-               
-               <ul>
-                   <li class="flex">
-                       <span><i class="fa-regular fa-clock"></i></span> 
-                       <span>144 Ore</span>
-                   </li>
-                   <li class="flex">
-                    <span><i class="fa-solid fa-money-bill-wave"></i></span> 
-                    <strong>€ 9,99</strong>
-                </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-eye"></i></span>
-                        <span>Presenza in home page</span>
-                   </li>
-                   <li class="flex">
-                       <span><i class="fa-solid fa-ranking-star"></i> </span>
-                       <span>Appari tra i primi risultati della ricerca</span></li>
-                   <li><a href="{{route('admin.doctors.paymentForm' , 3)}}" class="btn btn-primary">Acquista ora!</a></li>
-            
-               </ul>
-               <hr>
-            </div>
-    </div>
+        @endforeach
+    
     <div class="col-2 text-center pb-5">
 
         <a href="{{ route('admin.doctors.index') }}" class="btn btn-warning"><i
